51cb77bdb3284aa7b1897d6f9123770485f98038
brunokreutz <kreutz.dev@gmail.com>
2021-06-03 17:21:04 -0300

    feat: Adds google analytics data collection

--- source/js/tagueamento.js
+++ source/js/tagueamento.js
@@ -4,3 +4,33 @@
 // O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+ga('send', 'pageview');
+
+document.getElementsByClassName('menu-lista-contato')[0].addEventListener('click', function() {
+    console.log('oi');
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'menu',
+        eventAction: 'entre_em_contato',
+        eventLabel: 'link_externo'
+    });
+});
+
+document.getElementsByClassName('menu-lista-download')[0].addEventListener('click', function() {
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'menu',
+        eventAction: 'download_pdf',
+        eventLabel: 'download_pdf'
+    });
+});
+
+jQuery('.cards-montadoras').on('click tap', '.card-montadoras', function() {
+    let title = jQuery(this).data().name;
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'analise',
+        eventAction: 'ver_mais',
+        eventLabel: title
+    });
+});
